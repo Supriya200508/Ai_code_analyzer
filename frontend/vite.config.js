d618398fb63d3c17e1d16d944f3666d935c2cd75
Supriya200508 <supriyadec08@gmail.com>
2026-06-15 20:00:12 +0530

    remove invalid vite proxy config

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -3,13 +3,4 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
-  server: {
-    port: 5173,
-    proxy: {
-      "/api": {
-        target: import.meta.env.VITE_API_URL1,
-        changeOrigin: true,
-      },
-    },
-  },
 });
